2a4b41d9b19800bd42d1d918236a292936a66e89
preethi-0117 <preethipriti01@gmail.com>
2024-05-02 18:12:05 +0530

    fix:All components format changed

--- src/App.js
+++ src/App.js
@@ -10,28 +10,28 @@ import ShowResult from "./Finance/ShowResult";
 import { useEffect } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./Finance/FirebaseConfig";
-import { setuserdata, setIsLogin } from "./Finance/Redux-Toolkit/slices/RegLogCounter";
+import {
+  setuserdata,
+  setIsLogin,
+} from "./Finance/Redux-Toolkit/slices/RegLogCounter";
 import { useSelector, useDispatch } from "react-redux";
 
 function App() {
-  
-  const isLogin = useSelector((state)=> state.regisLogin.isLogin)
-  const userdata = useSelector((state) => state.regisLogin.userdata)
-  const dispatch = useDispatch()
+  const isLogin = useSelector((state) => state.regisLogin.isLogin);
+  const userdata = useSelector((state) => state.regisLogin.userdata);
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    checkAuth()
-  },[])
+  useEffect(() => {
+    checkAuth();
+  }, []);
 
   const checkAuth = async () => {
-      await onAuthStateChanged(auth, (currentuser)=> {
-          dispatch(setuserdata(currentuser))
-          dispatch(setIsLogin(true))
-        console.log(userdata)
-        }
-      )
-  }
-
+    await onAuthStateChanged(auth, (currentuser) => {
+      dispatch(setuserdata(currentuser));
+      dispatch(setIsLogin(true));
+      console.log(userdata);
+    });
+  };
 
   return (
     <div className="App">
@@ -39,9 +39,11 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-         {isLogin == true ? <Route path="/personaldetail" element={<PersonalDetail />} /> : null }
+          {isLogin == true ? (
+            <Route path="/personaldetail" element={<PersonalDetail />} />
+          ) : null}
           <Route path="/category" element={<Category />} />
-          <Route path="/financedetail" element={<FinanceDetail />} /> 
+          <Route path="/financedetail" element={<FinanceDetail />} />
           <Route path="/showresult" element={<ShowResult />} />
         </Routes>
       </BrowserRouter>
--- src/Finance/PersonalDetail/index.js
+++ src/Finance/PersonalDetail/index.js
@@ -1,160 +1,257 @@
-import { useSelector, useDispatch } from "react-redux"
-import { setPersonalInfo } from "../Redux-Toolkit/slices/PersonalDetailCounter"
+import { useSelector, useDispatch } from "react-redux";
+import { setPersonalInfo } from "../Redux-Toolkit/slices/PersonalDetailCounter";
 import { useNavigate } from "react-router-dom";
 import { db } from "../FirebaseConfig";
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc } from "firebase/firestore";
 import { Firestore } from "firebase/firestore";
 import { setuserdata } from "../Redux-Toolkit/slices/RegLogCounter";
 
+export default function PersonalDetail() {
+  const userdata = useSelector((state) => state.regisLogin.userdata);
+  const personalInfo = useSelector(
+    (state) => state.personalDetail.personalInfo
+  );
+  const dispatch = useDispatch();
 
-export default function PersonalDetail (){
+  const navigate = useNavigate();
 
-    const userdata = useSelector((state) => state.regisLogin.userdata)
-    const personalInfo = useSelector((state) => state.personalDetail.personalInfo)
-    const dispatch = useDispatch()
+  const handlePersonalDetail = async () => {
+    if (
+      personalInfo.firstName == "" ||
+      personalInfo.lastName == "" ||
+      personalInfo.fatherName == "" ||
+      personalInfo.Age == "" ||
+      personalInfo.maritalStatus == "" ||
+      personalInfo.Gender == "" ||
+      personalInfo.Email == "" ||
+      personalInfo.Address.District == "" ||
+      personalInfo.Address.City == "" ||
+      personalInfo.Address.pinCode == "" ||
+      personalInfo.Contact == ""
+    ) {
+      alert("Please fill empty fields");
+    } else {
+      await addDoc(collection(db, "personalDetails"), {
+        ...personalInfo,
+        uid: userdata.uid,
+      });
+      alert("Personal datas successfully submitted");
+      navigate("/category");
+    }
+  };
 
-    const navigate= useNavigate()
+  return (
+    <>
+      <h1>Basic Information</h1>
 
-
-    const handlePersonalDetail = async () =>{
-
-        if(personalInfo.firstName == "" || personalInfo.lastName == "" || personalInfo.fatherName == "" || personalInfo.Age == "" || personalInfo.maritalStatus == "" || personalInfo.Gender == "" || personalInfo.Email == "" || personalInfo.Address.District == "" || personalInfo.Address.City == "" || personalInfo.Address.pinCode == ""|| personalInfo.Contact == ""){
-            alert("Please fill empty fields")
-        }
-        else
-            {
-                
-               await addDoc(collection(db, "personalDetails"),{...personalInfo,uid:userdata.uid});
-                alert("Personal datas successfully submitted")
-                navigate("/category")
-                 }
-                
-        } 
-       
-    
-
-    return(
-        <>
-        <h1>Basic Information</h1>
-
-        <div>
+      <div>
         <label>First Name</label>
-        <input type="text" placeholder="Enter your first name" required onKeyUp={(e)=>
-               dispatch(setPersonalInfo({...personalInfo, firstName:e.target.value}))}></input>
-        </div>
+        <input
+          type="text"
+          placeholder="Enter your first name"
+          required
+          onKeyUp={(e) =>
+            dispatch(
+              setPersonalInfo({ ...personalInfo, firstName: e.target.value })
+            )
+          }
+        ></input>
+      </div>
 
-        <div>
+      <div>
         <label>Last Name</label>
-        <input type="text" placeholder="Enter your last name" required onKeyUp={(e)=>
-               dispatch(setPersonalInfo({...personalInfo, lastName:e.target.value}))}></input>
-        </div>
+        <input
+          type="text"
+          placeholder="Enter your last name"
+          required
+          onKeyUp={(e) =>
+            dispatch(
+              setPersonalInfo({ ...personalInfo, lastName: e.target.value })
+            )
+          }
+        ></input>
+      </div>
 
-        <div>
+      <div>
         <label>Father Name</label>
-        <input type="text" placeholder="Enter your father name" required onKeyUp={(e)=>
-               dispatch(setPersonalInfo({...personalInfo, fatherName:e.target.value}))}></input>
-        </div>
+        <input
+          type="text"
+          placeholder="Enter your father name"
+          required
+          onKeyUp={(e) =>
+            dispatch(
+              setPersonalInfo({ ...personalInfo, fatherName: e.target.value })
+            )
+          }
+        ></input>
+      </div>
 
-        <div>
+      <div>
         <label>Age</label>
-        <input type="number" placeholder="Enter your age" required onKeyUp={(e)=>
-               dispatch(setPersonalInfo({...personalInfo, Age:e.target.value}))}></input>
-        </div>
+        <input
+          type="number"
+          placeholder="Enter your age"
+          required
+          onKeyUp={(e) =>
+            dispatch(setPersonalInfo({ ...personalInfo, Age: e.target.value }))
+          }
+        ></input>
+      </div>
 
-
-        <div>
+      <div>
         <label>Marital Status</label>
-    
-    <select onChange={(e)=>dispatch(setPersonalInfo({...personalInfo, maritalStatus:e.target.value}))}>
-    <option>Select marital status</option>
-    <option>Married</option>
-    <option>Unmarried</option>
-    </select>
 
-    </div>
+        <select
+          onChange={(e) =>
+            dispatch(
+              setPersonalInfo({
+                ...personalInfo,
+                maritalStatus: e.target.value,
+              })
+            )
+          }
+        >
+          <option>Select marital status</option>
+          <option>Married</option>
+          <option>Unmarried</option>
+        </select>
+      </div>
 
-
-        <div>
+      <div>
         <label>Gender</label>
-    
-    <select onChange={(e)=>dispatch(setPersonalInfo({...personalInfo, Gender:e.target.value}))}>
-    <option>Select Gender</option>
-    <option>Male</option>
-    <option>Female</option>
-    </select>
-    </div>
 
-        <div>
+        <select
+          onChange={(e) =>
+            dispatch(
+              setPersonalInfo({ ...personalInfo, Gender: e.target.value })
+            )
+          }
+        >
+          <option>Select Gender</option>
+          <option>Male</option>
+          <option>Female</option>
+        </select>
+      </div>
+
+      <div>
         <label>Email</label>
-        <input type="email" placeholder="Enter your email" required onKeyUp={(e)=>
-               dispatch(setPersonalInfo({...personalInfo, Email:e.target.value}))}></input>
-        </div>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          required
+          onKeyUp={(e) =>
+            dispatch(
+              setPersonalInfo({ ...personalInfo, Email: e.target.value })
+            )
+          }
+        ></input>
+      </div>
 
-        <div>
+      <div>
         <h5>Address</h5>
 
         <label>District</label>
-    
-    <select onChange={(e)=>dispatch(setPersonalInfo({...personalInfo, Address:{...personalInfo.Address,District:e.target.value}}))}>
-    <option>Select District</option>
-    <option>Ariyalur</option>
-    <option>Chennai</option>
-    <option>Coimbatore</option>
-    <option>Cuddalore</option>
-    <option>Dharmapuri</option>
-    <option>Dindigul</option>
-    <option>Erode</option>
-    <option>Kallakurichi</option>
-    <option>Kanchipuram</option>
-    <option>Kanyakumari</option>
-    <option>Karur</option>
-    <option>Krishnagiri</option>
-    <option>Madurai</option>
-    <option>Nagapattinam</option>
-    <option>Namakkal</option>
-    <option>Nilgiris</option>
-    <option>Perambalur</option>
-    <option>Pudukkottai</option>
-    <option>Ramanathapuram</option>
-    <option>Ranipet</option>
-    <option>Salem</option>
-    <option>Sivaganga</option>
-    <option>Tenkasi</option>
-    <option>Thanjavur</option>
-    <option>Theni</option>
-    <option>Thoothukudi</option>
-    <option>Tiruchirappalli</option>
-    <option>Tirunelveli</option>
-    <option>Tirupathur</option>
-    <option>Tiruppur</option>
-    <option>Tiruvallur</option>
-    <option>Tiruvannamalai</option>
-    <option>Tiruvannamalai</option>
-    <option>Tiruvarur</option>
-    <option>Vellore</option>
-    <option>Viluppuram</option>
-    <option>Virudhunagar</option>
-    </select>
 
-    <label>City</label>
-    <input type="text" placeholder="Enter your City" required onKeyUp={(e)=>
-               dispatch(setPersonalInfo({...personalInfo, Address:{...personalInfo.Address,City:e.target.value}}))}></input>
+        <select
+          onChange={(e) =>
+            dispatch(
+              setPersonalInfo({
+                ...personalInfo,
+                Address: { ...personalInfo.Address, District: e.target.value },
+              })
+            )
+          }
+        >
+          <option>Select District</option>
+          <option>Ariyalur</option>
+          <option>Chennai</option>
+          <option>Coimbatore</option>
+          <option>Cuddalore</option>
+          <option>Dharmapuri</option>
+          <option>Dindigul</option>
+          <option>Erode</option>
+          <option>Kallakurichi</option>
+          <option>Kanchipuram</option>
+          <option>Kanyakumari</option>
+          <option>Karur</option>
+          <option>Krishnagiri</option>
+          <option>Madurai</option>
+          <option>Nagapattinam</option>
+          <option>Namakkal</option>
+          <option>Nilgiris</option>
+          <option>Perambalur</option>
+          <option>Pudukkottai</option>
+          <option>Ramanathapuram</option>
+          <option>Ranipet</option>
+          <option>Salem</option>
+          <option>Sivaganga</option>
+          <option>Tenkasi</option>
+          <option>Thanjavur</option>
+          <option>Theni</option>
+          <option>Thoothukudi</option>
+          <option>Tiruchirappalli</option>
+          <option>Tirunelveli</option>
+          <option>Tirupathur</option>
+          <option>Tiruppur</option>
+          <option>Tiruvallur</option>
+          <option>Tiruvannamalai</option>
+          <option>Tiruvannamalai</option>
+          <option>Tiruvarur</option>
+          <option>Vellore</option>
+          <option>Viluppuram</option>
+          <option>Virudhunagar</option>
+        </select>
 
-    <label>Pin code</label>
-    <input type="text" placeholder="Enter your Pincode" required onKeyUp={(e)=>
-               dispatch(setPersonalInfo({...personalInfo, Address:{...personalInfo.Address,pinCode:e.target.value}}))}></input>
-     </div>
-     
-    <div>
+        <label>City</label>
+        <input
+          type="text"
+          placeholder="Enter your City"
+          required
+          onKeyUp={(e) =>
+            dispatch(
+              setPersonalInfo({
+                ...personalInfo,
+                Address: { ...personalInfo.Address, City: e.target.value },
+              })
+            )
+          }
+        ></input>
+
+        <label>Pin code</label>
+        <input
+          type="text"
+          placeholder="Enter your Pincode"
+          required
+          onKeyUp={(e) =>
+            dispatch(
+              setPersonalInfo({
+                ...personalInfo,
+                Address: { ...personalInfo.Address, pinCode: e.target.value },
+              })
+            )
+          }
+        ></input>
+      </div>
+
+      <div>
         <label>Contact</label>
-        <input type="text" placeholder="Enter your contact number" required onKeyUp={(e)=>
-               dispatch(setPersonalInfo({...personalInfo, Contact:e.target.value}))}></input>
+        <input
+          type="text"
+          placeholder="Enter your contact number"
+          required
+          onKeyUp={(e) =>
+            dispatch(
+              setPersonalInfo({ ...personalInfo, Contact: e.target.value })
+            )
+          }
+        ></input>
       </div>
 
-<div>
-      <button type="button" onClick={handlePersonalDetail}>Next</button>
+      <div>
+        <button type="button" onClick={handlePersonalDetail}>
+          Next
+        </button>
       </div>
-
-        </>
-    )
+    </>
+  );
 }
