76e08df96981c80d4d1831adde1f0be94c1d8932
suthanks2000 <suthanks2000@gmail.com>
2024-05-08 14:55:44 +0530

    change vehicleloan submit function

--- src/Finance/FinanceDetail/PersonalLoanDetails.js
+++ src/Finance/FinanceDetail/PersonalLoanDetails.js
@@ -32,13 +32,39 @@ export default function PersonalLoanDetail() {
   const handlePersonalLoanDetail = async () => {
 
     
+    if (
+      plLoanInfo.employmentType == "" ||
+      plLoanInfo.placeOfWork == "" ||
+      plLoanInfo.jobTitle == "" ||
+      plLoanInfo.propertyStatus == "" ||
+      plLoanInfo.durationOfStayCurrentAddress.months == "" ||
+      plLoanInfo.durationOfStayCurrentAddress.years == "" ||
+      plLoanInfo.addressProof == "" ||
+      plLoanInfo.OHPFavorOf == "" ||
+      plLoanInfo.yearsEmployed == "" ||
+      plLoanInfo.monthlyNetIncome == "" ||
+      plLoanInfo.monthlyExpenses == "" ||
+      plLoanInfo.civilIssue == "" ||
+      plLoanInfo.purposeOfPersonalLoan == "" ||
+      plLoanInfo.loanAmount == "" ||
+      plLoanInfo.interest == "" ||
+      plLoanInfo.tenure.years == "" ||
+      plLoanInfo.tenure.months == "" ||
+      plLoanInfo.emi == ""
+    ) {
+      alert("Please fill empty fields");
+    } else {
       await addDoc(collection(db, "personelLoanDetails"), {
         ...plLoanInfo,
         uId: userdata.uid,
       });
+      alert("Personal datas successfully submitted");
       dispatch(setPlLoanInfo({ ...plLoanInfo, emi: emiValue }));
       console.log(plLoanInfo.LoanType);
       navigate("/showresult");
+    }
+  
+
     
   };
 useEffect(()=>{
