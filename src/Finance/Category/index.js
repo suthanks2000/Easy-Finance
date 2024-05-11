21d8c41803d3c11e095e5c7eec6280eb06665eb1
disondis <diaondis5@gmail.com>
2024-05-11 12:26:37 +0530

    feat changingthe emi componunt

--- src/Finance/Category/index.js
+++ src/Finance/Category/index.js
@@ -1,5 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';  
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { Link } from 'react-router-dom';
+
 
 
 export default function Category (){
