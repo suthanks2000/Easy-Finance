76874f9677e20e7bb5638f1e300e1443e745bc61
preethi-0117 <preethipriti01@gmail.com>
2024-05-22 12:50:44 +0530

    navbar component created

--- src/Finance/Category/index.js
+++ src/Finance/Category/index.js
@@ -1,63 +1,20 @@
  import { Link } from 'react-router-dom';
-
+import Navbar from '../Navbar';
 export default function Category (){
   
     return(
-       <>
-  <nav className="navbar sticky-top navbar-expand-lg  navbar-dark bg-dark">
-  <Link className="navbar-brand fs-3" href="#">Easy Finance</Link>
-  <button className="navbar-toggler shadow-none border-0" type="button" data-toggle="collapse" data-target="#myNavbar" aria-controls="myNavbar" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="myNavbar">
-    <ul className="navbar-nav justify-content-evenly flex-grow-1 pe-1">
-    <li className="nav-item">
-        <Link className="nav-link" to={'/personaldetail'}>Personal Detail</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link active" to={'/category'}>category</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to={'/emicalculator'}>EMI Calulator</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to={'/loandatas'}>Loan Datas</Link>
-      </li>
-      <li class="nav-item dropdown">
-        <Link class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Loan List
-        </Link>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <Link class="dropdown-item" to={'/personalloandetail'}>Personal Loan</Link>
-          <Link class="dropdown-item" to={'/personalloandetail'}>Home  Loan</Link>
-          <Link class="dropdown-item" to={'/personalloandetail'}>Vehicle Loan</Link>
-        </div>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" href="#">Help</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" href="#">Contact</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" href="#">Enquiries</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" href="#">About</Link>
-      </li>
-      
-    </ul>
-  </div>
-  <form className="d-flex">
-    <input type="text" className="form-control me-2" placeholder="Search"/>
-    <button type="button" className="btn btn-primary rounded-pill">Search</button>
-  </form>
-</nav>
-    <div>
-    <h1 className='h1 mt-5'>Category</h1>
-    </div>
+      <>
+           <Navbar/>
+         <h1>Category</h1>
+    </>
+    )
+  }
+
+//     <div>
+//     <h1 className='h1 mt-5'>Category</h1>
+//     </div>
    
-<div className="col d-flex justify-content-center mt-5"
+{/* <div className="col d-flex justify-content-center mt-5"
 style={{display:"flex",gap:"170px"}}>
     
     <Link to={"/loans/personalloan"} style={{ textDecoration: 'none' }}>
@@ -101,15 +58,6 @@ style={{display:"flex",gap:"170px"}}>
   </div>
 </div>
 </Link>
-</div>
-
-      
-</>
-    )
-}
-
-
-
-
+</div> */}
 
 
