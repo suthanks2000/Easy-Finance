4aa3c742398b303ba16635759bc1b93de395f1c3
preethi-0117 <preethipriti01@gmail.com>
2024-06-18 10:13:24 +0530

    login page, personal detail edit page, personal detail register page ui completed

--- src/Finance/PersonalDetail/index.js
+++ src/Finance/PersonalDetail/index.js
@@ -6,6 +6,8 @@ import { useDispatch } from "react-redux";
 import { Button } from "react-bootstrap";
 import Spinner from "react-bootstrap/Spinner";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Card } from "react-bootstrap";
 
 export default function PersonalDetail() {
   const userdata = useSelector((state) => state.regisLogin.userdata);
@@ -15,11 +17,14 @@ export default function PersonalDetail() {
   const [filterData, setFilterData] = useState({});
   const [spinner, setSpinner] = useState(true);
   const [editPersonalData, setEditPersonalData] = useState({});
+  const [successEdit,setSuccessEdit]=useState(false)
+  const Navigate = useNavigate()
 
   const uid = localStorage.getItem("loginUserId");
 
   const token = localStorage.getItem("Token");
   console.log(token);
+
 useEffect(() => {
   getUserPersonalData();
   
@@ -42,41 +47,46 @@ const getUserPersonalData = () => {
     });
 };
 
+const handleOnkeyup = (ele)=>{
+  if(ele.target.value == "Select District"){
+    alert("Please select others")
+  }
+  else{setUsersData({...usersData,[ele.target.name]:ele.target.value})
+  console.log("usersdata",usersData)
+}
+        
+       
+ }      
 
+ 
 
-   function handleEdit(user) {
-      setFilterData(user);
-      setEditData(true);
+  function handleExit() {
+    Navigate("/category")
   }
 
-  function exitFromEdit() {
-    setEditData(false);
-  }
 
-  function exitFromEdit(){
-    setEditData(false)
-  }
 
   const handleUpdateDetail = () => {
     const headers = { 'Authorization': `Bearer ${token}` };
     let formData = new FormData();
   
-    formData.append("first_name", filterData.first_name);
-    formData.append("last_name", filterData.last_name);
-    formData.append("father_name", filterData.father_name);
-    formData.append("age", filterData.age);
-    formData.append("gender", filterData.gender);
-    formData.append("marital_status", filterData.marital_status);
-    formData.append("district", filterData.district);
-    formData.append("city", filterData.city);
-    formData.append("pincode", filterData.pincode);
-    formData.append("contact", filterData.contact);
+    formData.append("first_name", usersData.first_name);
+    formData.append("last_name", usersData.last_name);
+    formData.append("father_name", usersData.father_name);
+    formData.append("age", usersData.age);
+    formData.append("gender", usersData.gender);
+    formData.append("marital_status", usersData.marital_status);
+    formData.append("district", usersData.district);
+    formData.append("city", usersData.city);
+    formData.append("pincode", usersData.pincode);
+    formData.append("contact", usersData.contact);
 
   
     axios.put(`https://PreethiJP.pythonanywhere.com/editPersonalData/${uid}`, formData, { headers })
       .then(() => {
         console.log("Personal details updated successfully");
-        setEditData(false);
+        console.log("personaldetail",usersData)
+        setSuccessEdit(true)
         getUserPersonalData();
       })
       .catch((error) => {
@@ -84,124 +94,79 @@ const getUserPersonalData = () => {
       });
   };
 
+
   return (
     <>
-      {!editData ? (
-        <>
-          <center>
-            <h1>Welcome to Personal Detail Page</h1>
-            <div>
-              <p>
-                Full Name: {usersData.first_name} {usersData.last_name}
+    { !successEdit?
+    <>
+    <div className="col-lg-9 mt-lg-0 mt-4 mx-auto">
+      
+      <div className="card card-body" id="profile">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-sm-auto col-4">
+            {/* <div className="avatar avatar-xl position-relative">
+              <img src="../../../assets/img/team-3.jpg" alt="bruce" className="w-100 border-radius-lg shadow-sm"/>
+            </div> */}
+          </div>
+          <div className="col-sm-auto col-8 my-auto">
+            <div className="h-100">
+              <h5 className="mb-1 font-weight-bolder">
+               {usersData.first_name} {usersData.last_name}
+              </h5>b
+              <p className="mb-0 font-weight-bold text-sm">
+                { usersData.contact }
               </p>
-              <p>Father Name: {usersData.father_name}</p>
-              <p>Age: {usersData.age}</p>
-              <p>Gender: {usersData.gender}</p>
-              <p>Marital status: {usersData.marital_status}</p>
-              <p>District: {usersData.district}</p>
-              <p>City: {usersData.city}</p>
-              <p>Pincode: {usersData.pincode}</p>
-              <p>Contact: {usersData.contact}</p>
-              <button type="button" onClick={() => handleEdit(usersData)}>
-                Edit
-              </button>
             </div>
-          </center>
-        </>
-      ) : null}
-      <>
-        <Modal show={editData}>
-          <center>
-            <Modal.Header>
-              <Modal.Title>
-                {filterData.first_name} {filterData.last_name}
-              </Modal.Title>
-              <Button className="btn-close" onClick={exitFromEdit}></Button>
-            </Modal.Header>
-            <Modal.Body>
-              <h1>Edit Your Data</h1>
-              <div>
-                <label>First Name</label>
-                <input
-                  type="text"
-                  name="first_name"
-                  defaultValue={filterData.first_name}
-                  onChange={(e) => handleOnkeyup(e)}
-                />
-              </div>
-              <div>
-                <label>Last Name</label>
-                <input
-                  type="text"
-                  name="last_name"
-                  defaultValue={filterData.last_name}
-                  onChange={(e) => handleOnkeyup(e)}
-                />
-              </div>
-              <div>
-                <label>Father Name</label>
-                <input
-                  type="text"
-                  name="father_name"
-                  defaultValue={filterData.father_name}
-                  onChange={(e) => handleOnkeyup(e)}
-                />
-              </div>
-              <div>
-                <label>Age</label>
-                <input
-                  type="number"
-                  name="age"
-                  defaultValue={filterData.age}
-                  onChange={(e) => handleOnkeyup(e)}
-                />
-              </div>
-              <div>
-                <label>Gender</label>
-                <input
-                  type="radio"
-                  name="gender"
-                  value="male"
-                  defaultChecked={filterData.gender === "male"}
-                  onChange={(e) => handleOnkeyup(e)}
-                />{" "}
-                Male
-                <input
-                  type="radio"
-                  name="gender"
-                  value="female"
-                  defaultChecked={filterData.gender === "female"}
-                  onChange={(e) => handleOnkeyup(e)}
-                />{" "}
-                Female
-              </div>
-              <div>
-                <label>Marital Status</label>
-                <input
-                  type="radio"
-                  name="marital_status"
-                  value="married"
-                  defaultChecked={filterData.marital_status === "married"}
-                  onChange={(e) => handleOnkeyup(e)}
-                />{" "}
-                Married
-                <input
-                  type="radio"
-                  name="marital_status"
-                  value="unmarried"
-                  defaultChecked={filterData.marital_status === "unmarried"}
-                  onChange={(e) => handleOnkeyup(e)}
-                />{" "}
-                Unmarried
-              </div>
-
-              <div>
-                <label>District</label>
-                <select
-                  name="district"
-                  defaultValue={filterData.district}
-                  onChange={(e) => handleOnkeyup(e)}
-                >
+          </div>
+          
+        </div>
+      </div>
+      <div className="card mt-4" id="basic-info">
+        <div className="card-header border-0">
+          <h5>Personal Details</h5>
+        </div>
+        <div className="card-body pt-0">
+          <div className="row mb-3">
+            <div className="col-md-6">
+              <label className="form-label">First Name</label>
+              <input id="firstName" name="first_name" className="form-control" type="text" required defaultValue={usersData.first_name} onChange={(e)=> handleOnkeyup(e)}/>
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Last Name</label>
+              <input id="lastName" name="last_name" className="form-control" type="text" required defaultValue={usersData.last_name} onChange={(e)=> handleOnkeyup(e)}/>
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col-md-6">
+              <label className="form-label">Father Name</label>
+              <input id="firstName" name="father_name" className="form-control" type="text" required defaultValue={usersData.father_name} onChange={(e)=> handleOnkeyup(e)}/>
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Age</label>
+              <input id="lastName" name="age" className="form-control" type="text" required defaultValue={usersData.age} onChange={(e)=> handleOnkeyup(e)}/>
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col-md-6">
+              <label className="form-label">Gender</label>
+              <select className="form-control" name="gender" id="choices-gender" value={usersData.gender} onChange={(e)=> handleOnkeyup(e)}>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+              </select>
+            </div>
+            <div className="col-md-6">
+              <label className="form-label">Marital Status</label>
+              <select className="form-control" name="marital_status" id="choices-gender" value={usersData.marital_status} onChange={(e)=> handleOnkeyup(e)}>
+                <option value="married">Married</option>
+                <option value="unmarried">Unmarried</option>
+              </select>
+            </div>
+            
+          </div>
+          <div className="row mb-3">
+            <div className="col-6">
+            <label className="form-label">District</label>
+              <select className="form-control" name="district" id="choices-gender" value={usersData.district} onChange={(e)=> handleOnkeyup(e)}>
                   <option>Select District</option>
                   <option>Ariyalur</option>
                   <option>Chengalpattu</option>
@@ -240,45 +205,63 @@ const getUserPersonalData = () => {
                   <option>Vellore</option>
                   <option>Viluppuram</option>
                   <option>Virudhunagar</option>
-                </select>
-              </div>
-
-              <div>
-                <label>City</label>
-                <input
-                  type="text"
-                  name="city"
-                  defaultValue={filterData.city}
-                  onChange={(e) => handleOnkeyup(e)}
-                />
-              </div>
-              <div>
-                <label>Pincode</label>
-                <input
-                  type="text"
-                  name="pincode"
-                  defaultValue={filterData.pincode}
-                  onChange={(e) => handleOnkeyup(e)}
-                />
-              </div>
-              <div>
-                <label>Contact Number</label>
-                <input
-                  type="text"
-                  name="contact"
-                  defaultValue={filterData.contact}
-                  onChange={(e) => handleOnkeyup(e)}
-                />
-              </div>
-            </Modal.Body>
-            <Modal.Footer>
-              <Button className="btn btn-info" onClick={handleUpdateDetail}>
-                Update
-              </Button>
-            </Modal.Footer>
-          </center>
-        </Modal>
-      </>
-    </>
-  );
+              </select>
+            </div>
+            <div className="col-6">
+              <label className="form-label">City</label>
+              <input id="confirmation" name="city" className="form-control" type="email" defaultValue={usersData.city} onChange={(e)=> handleOnkeyup(e)}/>
+            </div>
+          </div>
+          <div className="row mb-3">
+            <div className="col-6">
+              <label className="form-label">Pincode</label>
+              <input id="location" name="pincode" className="form-control" type="text" defaultValue={usersData.pincode} onChange={(e)=> handleOnkeyup(e)}/>
+            </div>
+            <div className="col-6">
+              <label className="form-label">Phone Number</label>
+              <input id="phone" name="contact" className="form-control" type="number" defaultValue={usersData.contact} onChange={(e)=> handleOnkeyup(e)}/>
+            </div>
+          </div>
+          <div className="row">
+        <div className="col-10">
+          <button type="button" className="btn btn-primary float-end" onClick={handleUpdateDetail}>Save Changes</button>
+        </div>
+        <div className="col-2">
+          <button type="button" className="btn btn-warning float-end" onClick={handleExit}>Cancel</button>
+        </div>
+      </div>
+          
+        </div>
+      </div>
+    </div>
+  </>
+  :
+  <>
+  
+  <Card>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <br />
+      <Card>
+        <Card.Body>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+        <Card.Img variant="bottom" src="holder.js/100px180" />
+      </Card>
+  </>
+  }
+  </>
+)
 }
+
+
+  
