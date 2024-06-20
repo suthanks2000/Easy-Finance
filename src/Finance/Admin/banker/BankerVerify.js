d5bdfc780098afbd5aa30d295b277ee33b1f0f59
suthanks2000 <suthanks2000@gmail.com>
2024-06-20 20:09:26 +0530

    fix:something clear

--- src/Finance/Admin/banker/BankerVerify.js
+++ src/Finance/Admin/banker/BankerVerify.js
@@ -24,7 +24,7 @@ const BankerVerify = () => {
    
     const res = await axios.put(
 
-      `https://PreethiJP.pythonanywhere.com/updateStatus?bankerToken=${token}&bankerEmail=${checkEmail}`,
+      `https://suthanks.pythonanywhere.com/updateStatus?bankerToken=${token}&bankerEmail=${checkEmail}`,
 
       formData
   ).then((response) => {
@@ -63,7 +63,7 @@ const changeStatusRegister = async () => {
     let formData = new FormData()
     formData.append("statusUpdate", updateStatus)
     
-    try {const res = await axios.put(`https://PreethiJP.pythonanywhere.com/updateVerifiedBanker/${checkEmail}`, formData)
+    try {const res = await axios.put(`https://suthanks.pythonanywhere.com/updateVerifiedBanker/${checkEmail}`, formData)
 
         console.log(res.data) 
         
